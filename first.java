beddbd5541e4bc37d5cfd6c073cd385b3e00d5cd
Anushka1826 <91601570+Anushka1826@users.noreply.github.com>
2022-06-02 21:52:54 +0530

    Add files via upload

--- first.java
+++ first.java
@@ -1,19 +1,152 @@
-class Animal {
-    void eat() {
-        System.out.println("eating...");
-    }
+
+abstract class Bharatvanshi {
+    abstract void fight();
 }
 
-class Dog extends Animal {
-    void bark() {
-        System.out.println("barking...");
+class Kauravs extends Bharatvanshi
+
+{
+
+    protected int kindness;
+
+    protected int obedience;
+
+    public Kauravs() {
+        kindness = obedience = 0;
     }
+
+    public void SetKindness(int x) {
+        kindness = x;
+    }
+
+    public void SetObedience(int x) {
+        obedience = x;
+    }
+
+    public int GetKindness() {
+        return (kindness);
+    }
+
+    public int GetObedience() {
+        return (obedience);
+    }
+
+    @Override
+    public void fight()
+
+    {
+
+        System.out.println(" Kauravs fight ");
+
+    }
+
+    public void kind()
+
+    {
+
+        System.out.println(" kindness = " + kindness);
+
+    }
+
+    public void obey()
+
+    {
+
+        System.out.println(" obedience = " + obedience);
+
+    }
+
 }
 
-class first {
-    public static void main(String args[]) {
-        Dog d = new Dog();
-        d.bark();
-        d.eat();
+class Pandavs extends Bharatvanshi
+
+{
+
+    protected int kindness;
+
+    protected int obedience;
+
+    public Pandavs() {
+        kindness = obedience = 10;
     }
+
+    public void SetKindness(int x) {
+        kindness = x;
+    }
+
+    public void SetObedience(int x) {
+        obedience = x;
+    }
+
+    public int GetKindness() {
+        return (kindness);
+    }
+
+    public int GetObedience() {
+        return (obedience);
+    }
+
+    @Override
+    public void fight()
+
+    {
+
+        System.out.println(" Kauravs fight ");
+
+    }
+
+    public void kind()
+
+    {
+
+        System.out.println(" kindness = " + kindness);
+
+    }
+
+    public void obey()
+
+    {
+
+        System.out.println(" obedience = " + obedience);
+
+    }
+
+}
+
+class first
+
+{
+
+    public static void main(String args[])
+
+    {
+
+        Pandavs Arjun = new Pandavs();
+
+        Pandavs Bheem = new Pandavs();
+
+        int kindness = Arjun.GetKindness();
+
+        Bheem.SetKindness(kindness - 1);
+
+        Kauravs kauravs[] = new Kauravs[100];
+
+        Kauravs Vikran = new Kauravs();
+
+        Vikran.SetKindness(10);
+
+        Vikran.SetObedience(10);
+
+        kauravs[0] = Vikran;
+
+        for (int iLoop = 1; iLoop < 100; iLoop++)
+
+        {
+
+            kauravs[iLoop] = new Kauravs();
+
+        }
+
+    }
+
 }
--- second.java
+++ second.java
@@ -1,12 +1,39 @@
-class Mother {
-    void run() {
-        System.out.println("Hello JUET");
+
+interface MAD {
+    void learnCoding();
+
+    void learnProgrammingLanguage();
+
+    void contribute();
+}
+
+abstract class Student implements MAD {
+    @Override
+    public void learnCoding() {
+        System.out.println(
+                "Hi");
+    }
+
+    @Override
+    public void learnProgrammingLanguage() {
+        System.out.println(
+                "JUET");
     }
 }
 
-class second extends Mother {
-    public static void main(String args[]) {
-        second obj = new second();
-        obj.run();
+class Mobile extends Student {
+    @Override
+    public void contribute() {
+        System.out.println(
+                "MAD LAB");
+    }
+}
+
+public class second {
+    public static void main(String[] args) {
+        Mobile gfgStudent = new Mobile();
+        gfgStudent.learnCoding();
+        gfgStudent.learnProgrammingLanguage();
+        gfgStudent.contribute();
     }
 }
